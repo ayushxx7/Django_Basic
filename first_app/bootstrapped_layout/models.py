c25b85ccdbf404a25f253882fa1cf5b35ee57554
ayushxx7 <ayushxx7@gmail.com>
2019-12-16 23:20:09 +0530

    adding database functionality

--- first_app/bootstrapped_layout/models.py
+++ first_app/bootstrapped_layout/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Destination:
+class Destination(models.Model):
 
-	index : int
-	name : str
-	img: str
-	desc: str
-	price: int
+	name = models.CharField(max_length=100)
+	img = models.ImageField(upload_to='pics')
+	desc = models.TextField()
+	price = models.IntegerField()
 
 	
